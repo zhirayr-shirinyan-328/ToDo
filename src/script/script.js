9312f5b8064fda074214c7f20bbdbe6b32baa9f2
lys <annieway.hakobyan@gmail.com>
2020-01-21 11:20:37 -0800

    Added query selectors

---
+++ src/script/script.js
@@ -0,0 +1,11 @@
+const main = document.querySelectorAll('.main');
+const header = document.querySelectorAll('.header');
+const title = document.querySelectorAll('.header-title');
+const menu = document.querySelectorAll('.menu-icon-container');
+const dummyTask = document.querySelectorAll('.todo-item');
+// const test = document.querySelectorAll('.task-checkbox');
+// const test = document.querySelectorAll('.task-category-indicator');
+
+
+
+
